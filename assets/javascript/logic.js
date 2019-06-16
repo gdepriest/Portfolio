cfd54a484f53ce9d4363a08ef7fe664f4ea639aa
Genevieve DePriest <gdpparton@gmail.com>
2019-06-16 15:17:05 -0600

    made a few minor changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,7 +4,7 @@ var portfolioInfo = [
         name: "Babes in Brunchland",
         image: "assets/images/brunchlandScreencapture.png",
         url: "https://savannahisrael.github.io/BabesInBrunchland/index.html",
-        summary: "UDEN Bootcamp Project 1: An app that delivers brunch options (with a map!) based on zip code and price point.",
+        summary: "UDEN Bootcamp Project 1: An app that delivers brunch options (with maps!) based on zip code and price point.",
     },
     {
         name: "Turkey Trivia",
@@ -33,11 +33,27 @@ var portfolioInfo = [
    
 ]
 
+//Firebase
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCFQnQO4kHjqQRnu7SeWj-4kL9YIEaFbII",
+//     authDomain: "testing-testing-84a4d.firebaseapp.com",
+//     databaseURL: "https://testing-testing-84a4d.firebaseio.com",
+//     projectId: "testing-testing-84a4d",
+//     storageBucket: "testing-testing-84a4d.appspot.com",
+//     messagingSenderId: "813376100953",
+//     appId: "1:813376100953:web:5453e22a0437e313"
+// };
+//         // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// var database = firebase.database();
+
 //On click of portfolio button, slide down modal, display portfolio information
 
 $("#portfolio-btn").on("click", function() {
     $("#aboutMeCard").hide();
-    $(".modal").slideDown();
+    $(".modal").slideDown('slow');
     
     $("#modalTitle").text("Portfolio");
 
@@ -67,11 +83,11 @@ $("#portfolio-btn").on("click", function() {
        $(".modal-body").append(hRef);  
        
        function infoShow() {
-            $(cardOverlay).slideDown();
+            $(cardOverlay).slideDown('slow');
        };
 
        function infoHide() {
-           $(cardOverlay).slideUp();
+           $(cardOverlay).slideUp('slow');
        };
 
        $(hRef).hover(infoShow, infoHide);
@@ -92,7 +108,7 @@ $(".close-btn").on("click", function() {
 
 $("#contact-btn").on("click", function() {
     $("#aboutMeCard").hide();
-    $(".modal").slideDown();
+    $(".modal").slideDown('slow');
 
     $("#modalTitle").text("Contact Me");
 
@@ -126,6 +142,25 @@ $("#contact-btn").on("click", function() {
     var submitBtn = $("<button>").addClass("submit-btn btn btn-primary btn-sm").attr("type", "submit").text("Submit");
     $(".modal-footer").prepend(submitBtn);
 
-
 })
 
+$(".submit-btn").on("click", function(event) {
+    event.preventDefault();
+    
+    var nameInput = $("#nameInput").val().trim();
+    var emailInput = $("#emailInput").val().trim();
+    var textInput = $("#textInput").val().trim();
+
+    var newMessage = {
+        name: nameInput,
+        email: emailInput,
+        msg: textInput,
+    }
+
+    database.ref().push(newMessage);
+
+    $("#nameInput").val("");
+    $("#emailInput").val("");
+    $("#textInput").val("");    
+
+})
